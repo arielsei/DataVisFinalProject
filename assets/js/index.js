b4ab632ab1380625a1ac2a0489d1ef83b839e696
eliasmpw <eliasmpw@hotmail.com>
2018-11-17 19:15:54 +0100

    Buttons and popup to the side

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,54 @@
-
 window.onload = () => {
-    const map = new L.map('map', {
-        center: [51.505, -0.09],
-        zoom: 4
-    }).addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
+    const map = new L.map("map", {
+        center: [-6, -60],
+        zoom: 5
+    });
+    const layerOptions = {
+        maxZoom: 7
+    };
+    const customOption1 = L.Control.extend({
+        options: {
+            position: "bottomcenter"
+        },
+
+        onAdd: function(map) {
+            let container = L.DomUtil.get("option1");
+            container.onclick = () => {
+                document.getElementById("testContent").innerHTML =
+                    "This is description for Option 1";
+                document.getElementById("descriptions").style.display = "block";
+            };
+            return container;
+        }
+    });
+    const customOption2 = L.Control.extend({
+        options: {
+            position: "bottomcenter"
+        },
+
+        onAdd: function(map) {
+            let container = L.DomUtil.get("option2");
+            container.onclick = () => {
+                document.getElementById("testContent").innerHTML =
+                    "You selected Option 2";
+                document.getElementById("descriptions").style.display = "block";
+            };
+            return container;
+        }
+    });
+    L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        layerOptions
+    ).addTo(map);
+    option1 = L.DomUtil.get("option1");
+    option2 = L.DomUtil.get("option2");
+    const corners = map._controlCorners;
+    const container = map._controlContainer;
+    corners["bottomcenter"] = L.DomUtil.create(
+        "div",
+        "leaflet-bottom leaflet-horizontal-center",
+        container
+    );
+    map.addControl(new customOption2());
+    map.addControl(new customOption1());
 };
